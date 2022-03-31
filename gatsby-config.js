7007b489fb21af20854b364c99488ec56b731e43
nanobric <nano@juarezsupplies.com>
2022-03-31 15:48:03 -0600

    navrar logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Wine News",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "Simple wine news and tips to enjoy this wonderfull wine world",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
