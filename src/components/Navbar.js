959e3eeec6d5b3d6e31da8ff3d60cb973d1b4696
nanobric <nano@juarezsupplies.com>
2022-03-31 21:30:21 -0600

    navrar logo2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="sergio-logo" style={{ width: "88px" }} />
+              <img src={logo} alt="sergio-logo" style={{ width: "100%" }} />
             </Link>
             {/* Hamburger menu */}
             <div
